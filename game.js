f331dce9d5efa1b395d9e8c6c0dc5286ca58bf0f
Nathalie Spiller <nathalie.spiller@protonmail.ch>
2022-08-30 16:54:32 +0200

    console game, rock paper scissors

--- game.js
+++ game.js
@@ -1,1 +1,61 @@
-console.log("Hello World!");
+const options = ["rock", "paper", "scissors"]; //array with options for game, global variable as it is used in different functions
+
+function getComputerChoice(){
+    let computersChoice = options[Math.floor(Math.random()*3)]; // chooses one of the three options out of array at random
+    return computersChoice;
+}
+
+
+function getPlayerChoice(){ // change to prompt blabla and blbla.toLowerCase()
+    let playersChoice = prompt("Enter your choice of rock, paper, scissors :");
+    playersChoice = playersChoice.toLowerCase();
+    if (options.includes(playersChoice)){
+      return playersChoice;
+    } else{
+      console.log(`not a valid entry`); //??? is it possible to call the function again?
+      return null;
+    }
+}
+
+
+function playRound(playerSelection, computerSelection){
+  if (playerSelection == computerSelection){
+    return "draw";
+  }else if(playerSelection =="rock" && computerSelection =="paper"||playerSelection =="scissors" && computerSelection =="rock"||playerSelection =="paper" && computerSelection =="scissors"){
+    return "Computer wins";
+  } else{
+    return "Player wins";
+  }
+}
+
+
+
+function game(){
+  let numbRound = 0;
+  let numbCompWin = 0;
+  let numbPlayerWin = 0;
+  for (let i = 0; i<5; i++){
+
+    let actualRound = playRound(getPlayerChoice(),getComputerChoice());
+    console.log(actualRound);
+    if (actualRound == "Computer wins"){
+      numbCompWin++;
+      numbRound++;
+    }else if (actualRound =="Player wins"){
+      numbPlayerWin++;
+      numbRound++;
+    }else{
+      numbRound++;
+    }
+  }
+
+  if (numbCompWin < numbPlayerWin){
+    console.log(`You win the game! Computer has won ${numbCompWin} rounds and you have won ${numbPlayerWin} rounds.`)
+  } else if (numbCompWin > numbPlayerWin){
+    console.log(`Computer has won the game! Computer has won ${numbCompWin} rounds and you have won ${numbPlayerWin} rounds.`)
+  }else{
+    console.log(`It's a draw. Computer has won ${numbCompWin} rounds and you have won ${numbPlayerWin} rounds.`)
+  }
+}
+
+game();
